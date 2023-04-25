a3a3e2ce185aeb12ab48f9db732e6180bc98ede1
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-04-25 19:04:10 -0300

    Inicio da lógica de criação de salas por parte do cliente

--- CampoMinadoServer.cs
+++ CampoMinadoServer.cs
@@ -7,8 +7,47 @@ using System.Net.Sockets;
 
 class Campo_Minado_Server
 {
+    static int maxGameRooms = 4;
+    static List<GameRoom> gameRooms = new List<GameRoom>(0);
+
+    static TcpListener listener;
+
     static void Main(string[] args)
     {
+        int port = 6778;
 
+        Console.Write("Insira a porta: ");
+        try { port = int.Parse(Console.ReadLine()); }
+        catch (Exception e) { }
+
+        Console.Write("Insira o número máximo de salas: ");
+        try { maxGameRooms = int.Parse(Console.ReadLine()); }
+        catch (Exception e) { }
+
+        listener = new TcpListener(System.Net.IPAddress.Any, port);
+
+        Task.Run(ListenForConnections);
+    }
+
+    static void ListenForConnections()
+    {
+        while (gameRooms.Count() < maxGameRooms)
+        {
+            try
+            {
+                TcpClient player = listener.AcceptTcpClient();
+
+                Byte[] buffer = new Byte[512];
+                int size = player.GetStream().Read(buffer, 0, 512);
+
+                string[] content = Encoding.UTF8.GetString(buffer).Substring(0, size).Split('|');
+
+                gameRooms.Add(new GameRoom(listener, player, content[0], int.Parse(content[1])));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
     }
 }
