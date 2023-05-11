d363aaaccbb3e8874c0954c2593500569184ad7f
Rafael Silva <76886825+Rafael-Nunes-Silva@users.noreply.github.com>
2023-05-11 08:36:35 -0300

    Adicionei a função Disconnect

--- Player.cs
+++ Player.cs
@@ -53,6 +53,11 @@ public class Player
 
     }
 
+    public void Disconnect()
+    {
+        tcpConn.Close();
+    }
+
     bool QueueHasMessage(string msgName)
     {
         for (int i = 0; i < messageQueue.Count; i++)
@@ -145,49 +150,3 @@ public class Player
         return true;
     }
 }
-
-
-/*
-public class Player
-{
-    TcpClient tcpConn;
-    Connector connector;
-
-    string name = "UNKNOWN";
-    bool ready = false;
-
-    public Player(TcpClient tcpConn)
-    {
-        this.tcpConn = tcpConn;
-        connector = new Connector(tcpConn);
-
-        connector.WaitForMsg("NAME", (name) => { this.name = name[0]; });
-        connector.WaitForMsg("READY", (ready) => { this.ready = bool.Parse(ready[0]); }, false);
-    }
-
-    public Connector Connector()
-    {
-        return connector;
-    }
-
-    public void Disconnect()
-    {
-        tcpConn.Close();
-    }
-
-    public bool IsConnected()
-    {
-        return tcpConn.Connected;
-    }
-
-    public string GetName()
-    {
-        return name;
-    }
-
-    public bool IsReady()
-    {
-        return ready;
-    }
-}
-*/
